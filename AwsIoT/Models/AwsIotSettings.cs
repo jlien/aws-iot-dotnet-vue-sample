7bc41d2f362b690c55fd71ecbc24963966441de9
Jimmy Lien <jamesslien@gmail.com>
2018-10-04 08:17:12 -0500

    update iot settings

--- AwsIoT/Models/AwsIotSettings.cs
+++ AwsIoT/Models/AwsIotSettings.cs
@@ -4,13 +4,36 @@ namespace AwsIoT.Models
 {
     public static class AwsIotSettings
     {
-        public static string AccessKey =>
-            GetEnvironmentVariable("AwsIotAccessKey");
+        public static string SubscriberAccessKey =>
+        GetEnvironmentVariable("AwsIotSubscriberAccessKey");
 
-        public static string SecretKey =>
-            GetEnvironmentVariable("AwsIotSecretKey");
+        public static string SubscriberSecretKey =>
+        GetEnvironmentVariable("AwsIotSubscriberSecretKey");
+
+        public static string PublisherAccessKey =>
+        GetEnvironmentVariable("AwsIotPublisherAccessKey");
+
+        public static string PublisherSecretKey =>
+            GetEnvironmentVariable("AwsIotPublisherSecretKey");
+
 
         public static string HostName =>
             GetEnvironmentVariable("AwsIotHostName");
+
+        public static string Endpoint =>
+            GetEnvironmentVariable("AwsIotEndpoint");
+
+        public static string Region => Amazon.RegionEndpoint.USEast1.ToString();
+
+        public static string IdentityPoolId =>
+            GetEnvironmentVariable("AwsCognitoIdentityPoolId");
+
+        public static string CognitoProviderName =>
+        GetEnvironmentVariable("AwsCognitoProviderName");
+
+        public static string IdentityProviderName =>
+        GetEnvironmentVariable("AwsCognitoIdentityProviderName");
+
+        public static long TokenDuration => 43200;
     }
 }
